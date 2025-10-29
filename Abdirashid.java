a4e1d483890a05ae979f8286a5014a29f8a00dfa
khalidabdi10 <khalidabdirashid7000@gmail.com>
2025-10-29 15:08:52 +0300

    first commit

--- Abdirashid.java
+++ Abdirashid.java
@@ -7,7 +7,7 @@ class firstSon {
 class secondSon extends firstSon {
     void display() {
         super.displayActions();
-        System.out.println("He never cry");
+        System.out.println("He never cryss");
     }
 }
 
